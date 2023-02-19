c2dee700fb9972a6243797f09e76ed99714ab3b0
Patricknshimiyimana <lennoxmupi@gmail.com>
2023-02-19 22:13:02 +0200

    react portfolio

---
+++ src/components/LandingSection.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+import avatar from "../images/avatar.jpg";
+
+const greeting = "Hello, I am Patrick!";
+const bio1 = "A frontend developer";
+const bio2 = "specialized in React";
+
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#212936"
+  >
+    <VStack spacing={16}>
+      <VStack spacing={4} alignItems="center">
+        <Avatar src={avatar} size="2xl" name="Patrick" />
+        <Heading as="h4" size="md" noOfLines={1}>
+          {greeting}
+        </Heading>
+      </VStack>
+      <VStack spacing={6}>
+        <Heading as="h1" size="3xl" noOfLines={1}>
+          {bio1}
+        </Heading>
+        <Heading as="h1" size="3xl" noOfLines={1}>
+          {bio2}
+        </Heading>
+      </VStack>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
